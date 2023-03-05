81eeaf702711d2df33c2cee0b812b6b64e62a3b5
chovrio <1480733667@qq.com>
2023-03-05 14:52:52 +0800

    feat: delete dist file

---
+++ packages/chovrio/template/vue + typescript/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <h2>我是Home组件</h2>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style lang="less" scoped></style>
--- rollup.config.js
+++
@@ -1,3 +0,0 @@
-import path from 'path'
-import ts from '@rollup/plugin-typescript'
-import {} from 'node:url'
