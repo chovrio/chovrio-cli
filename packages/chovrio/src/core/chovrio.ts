a706bd617af4ea02eff5fe5567f99512f5048a6f
chovrio <1480733667@qq.com>
2023-03-05 15:18:15 +0800

    feat: the project command is created without adding the corresponding template

--- packages/chovrio/src/core/chovrio.ts
+++ packages/chovrio/src/core/chovrio.ts
@@ -1,9 +1,9 @@
 import { program } from 'commander'
 import { version } from '../../package.json'
-import init from './init'
+import create from './create'
 program.name('chovrio').description('CLI to JavaScript').version(version)
 program.option('--first').option('-s, --separator <char>').option('-w, --watch')
-init(program)
+create(program)
 program.parse()
 // const options = program.opts()
 // console.log(options, program.args)
---
+++ packages/chovrio/src/core/create.ts
@@ -0,0 +1,48 @@
+import inquirer from 'inquirer'
+import pc from 'picocolors'
+import ora from 'ora'
+import fs from 'fs-extra'
+// import { execa } from 'execa'
+import { resolve } from 'path'
+import type { Command } from 'commander'
+import { sleep } from '../utils/sleep'
+import { __dirname } from '../utils/__dirname'
+// 新建模板
+export default function init(program: Command) {
+  program
+    .command('create')
+    .description('create a project')
+    .argument('<string>', 'project name')
+    .action(async name => {
+      const { template } = await inquirer.prompt({
+        type: 'list',
+        name: 'template',
+        message: 'choose a template',
+        choices: [
+          'react',
+          'vue',
+          'react + typescript',
+          'vue + typescript',
+          'none'
+        ]
+      })
+      console.log(pc.green(`fetch template ${template}`))
+      const fetchingSpinning = ora(pc.blue(`fetch template`)).start()
+      fetchingSpinning.color = 'cyan'
+      fetchingSpinning.stop()
+      const copySpinner = ora(
+        pc.blue(`generate project by template...`)
+      ).start()
+      await sleep(3000)
+      const realPath = await fs.realpath(process.cwd())
+      const projectPath = realPath + '/' + name.toString()
+      await fs.copy(resolve(__dirname, `../template/${template}`), projectPath)
+      copySpinner.stop()
+      const installSpinner = ora(
+        pc.blue(`generate project by template...`)
+      ).start()
+      process.chdir(projectPath)
+      installSpinner.stop()
+      console.log(pc.green(`generate project by template`))
+    })
+}
